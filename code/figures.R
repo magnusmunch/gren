10143c535d5f0669c02d2253205736cbb06493e8
magnusmunch <magnus_munch@hotmail.com>
2019-02-19 10:14:28 +0100

    lymph node example worked out

--- code/figures.R
+++ code/figures.R
@@ -3,10 +3,10 @@
 # ---- boxplot_metabolomics_alzheimer_res1_auc ---- 
 library(pROC)
 library(Biobase)
-labels1 <- c(unique(as.character(feat$Platform))[-c(4, 5)], "Oxidative Stress")
 res1 <- read.table("results/metabolomics_alzheimer_res1.csv", 
                    stringsAsFactors=FALSE)
 load("data/ESetMbolCSFPR2.Rdata")
+feat <- fData(ESetMbolCSFPR2)
 pheno <- pData(ESetMbolCSFPR2)
 pheno.apoe <- pheno[(pheno$D_diag_name=="Probable AD" & pheno$APOE=="E4YES") |
                       (pheno$D_diag_name=="Subjectieve klachten" & 
@@ -17,6 +17,7 @@ auc1 <- sapply(res1[grepl("pred", rownames(res1)), ], function(s) {
   pROC::auc(alzheim.apoe, s)})
 psel1 <- colMeans(res1[grepl("psel", rownames(res1)), ])
 
+labels1 <- c(unique(as.character(feat$Platform))[-c(4, 5)], "Oxidative Stress")
 plot(psel1, auc1)
 boxplot(res1[grepl("psel", rownames(res1)), ])
 
@@ -24,18 +25,98 @@ barplot(rbind(fit1.gren1$lambdag$part, fit1.grridge$lambdamults$part),
         beside=TRUE)
 abline(h=1, lty=2)
 
+# ---- lines_rnaseq_lymph_node_metastasis_res1_auc ----
+library(CoRF)
+library(pROC)
+library(grpreg)
+load("../results/rnaseq_lymph_node_metastasis_fit1.Rdata")
+res1 <- read.table("../results/rnaseq_lymph_node_metastasis_res1.csv", 
+                   stringsAsFactors=FALSE)
 
-
-
+methods1 <- c("ridge", "grridge", "gren", "enet", "cmcp", "gel")
+pred1 <- as.matrix(res1[substr(rownames(res1), 1, 4)=="pred", ])
+psel1 <- as.matrix(res1[substr(rownames(res1), 1, 4)=="psel", ])
+ytest <- as.numeric(levels(RespValidation))[RespValidation]
 auc1 <- apply(pred1, 2, function(m) {pROC::auc(ytest, m)})
+plot.data1 <- lapply(c(t(outer(methods1[-c(1, 2)], 1:3, paste, sep=""))), 
+                     function(m) {aggregate(auc1[grepl(m, names(auc1))], list(
+                       psel=psel1[, grepl(m, colnames(psel1))]), mean)})
+cv.psel1 <- c(sum(coef(fit1.gren1$freq.model$groupreg, 
+                       s=fit1.gren1$lambda)[-1, ]!=0),
+              sum(coef(fit1.gren2$freq.model$groupreg, 
+                       s=fit1.gren2$lambda)[-1, ]!=0),
+              sum(coef(fit1.gren3$freq.model$groupreg, 
+                       s=fit1.gren3$lambda)[-1, ]!=0),
+              sum(coef(fit1.gren1$freq.model$regular, 
+                       s=fit1.gren1$lambda)[-1, ]!=0),
+              sum(coef(fit1.gren2$freq.model$regular, 
+                       s=fit1.gren2$lambda)[-1, ]!=0),
+              sum(coef(fit1.gren3$freq.model$regular, 
+                       s=fit1.gren3$lambda)[-1, ]!=0),
+              psel1[, grepl("cmcp1", colnames(psel1))][fit1.cmcp1$min],
+              psel1[, grepl("cmcp2", colnames(psel1))][fit1.cmcp2$min],
+              psel1[, grepl("cmcp3", colnames(psel1))][fit1.cmcp3$min],
+              psel1[, grepl("gel1", colnames(psel1))][fit1.gel1$min],
+              psel1[, grepl("gel2", colnames(psel1))][fit1.gel2$min],
+              psel1[, grepl("gel3", colnames(psel1))][fit1.gel3$min])
+col1 <- c(1:length(methods1))
+lty1 <- c(1:4)
 
-col1 <- as.numeric(factor(substr(names(auc1), 1, 
-                                 nchar(names(auc1)) - 1)[-c(1, 2)]))
-pch1 <- as.numeric(substr(names(auc1), nchar(names(auc1)), 
-                          nchar(names(auc1)))[-c(1, 2)])
-plot(as.numeric(psel1)[-c(1, 2)], auc1[-c(1, 2)], col=col1, pch=pch1,
-     xlab="Number of selected features", ylab="AUC")
-abline(h=auc1[c(1, 2)], col=c(5, 6), lty=2)
-methods1 <- c("ridge", "grridge", "enet", "gren", "cmcp", "gel")
-legend("topright", legend=c())
+par(mfrow=c(1, 2))
+plot(plot.data1[[1]], type="l", xlab="Number of selected features", ylab="AUC", 
+     main="a)", ylim=range(auc1), xlim=range(psel1), col=col1[3], lty=lty1[1])
+lines(plot.data1[[2]], col=col1[3], lty=lty1[2])
+lines(plot.data1[[3]], col=col1[3], lty=lty1[3])
+lines(plot.data1[[4]], col=col1[4], lty=lty1[1])
+lines(plot.data1[[5]], col=col1[4], lty=lty1[2])
+lines(plot.data1[[6]], col=col1[4], lty=lty1[3])
+lines(plot.data1[[7]], col=col1[5], lty=lty1[1])
+lines(plot.data1[[8]], col=col1[5], lty=lty1[2])
+lines(plot.data1[[9]], col=col1[5], lty=lty1[3])
+lines(plot.data1[[10]], col=col1[6], lty=lty1[1])
+lines(plot.data1[[11]], col=col1[6], lty=lty1[2])
+lines(plot.data1[[12]], col=col1[6], lty=lty1[3])
+abline(h=auc1[names(auc1)=="ridge"], col=col1[1], lty=lty1[4])
+abline(h=auc1[names(auc1)=="grridge"], col=col1[2], lty=lty1[4])
+points(t(sapply(1:length(plot.data1), function(m) {
+  subset(plot.data1[[m]], psel==cv.psel1[m])})), 
+  col=rep(col1[-c(1, 2)], each=3), pch=1)
+legend("bottomright", 
+       legend=c(methods1, expression(alpha==0.05, alpha==0.5, alpha==0.95),
+                "CV model"), 
+       fill=c(col1, rep(NA, 4)),
+       border=c(rep(1, length(methods1)), rep(NA, 4)), 
+       lty=c(rep(NA, length(methods1)), lty1, NA),
+       pch=c(rep(NA, length(methods1)), rep(NA, 1), 4),
+       seg.len=1, merge=TRUE)
+
+plot(plot.data1[[1]], type="l", xlab="Number of selected features", ylab="AUC", 
+     main="b)", ylim=c(0.63, max(auc1)), xlim=c(0, 500), col=col1[3], 
+     lty=lty1[1])
+lines(plot.data1[[2]], col=col1[3], lty=lty1[2])
+lines(plot.data1[[3]], col=col1[3], lty=lty1[3])
+lines(plot.data1[[4]], col=col1[4], lty=lty1[1])
+lines(plot.data1[[5]], col=col1[4], lty=lty1[2])
+lines(plot.data1[[6]], col=col1[4], lty=lty1[3])
+lines(plot.data1[[7]], col=col1[5], lty=lty1[1])
+lines(plot.data1[[8]], col=col1[5], lty=lty1[2])
+lines(plot.data1[[9]], col=col1[5], lty=lty1[3])
+lines(plot.data1[[10]], col=col1[6], lty=lty1[1])
+lines(plot.data1[[11]], col=col1[6], lty=lty1[2])
+lines(plot.data1[[12]], col=col1[6], lty=lty1[3])
+abline(h=auc1[names(auc1)=="ridge"], col=col1[1], lty=lty1[4])
+abline(h=auc1[names(auc1)=="grridge"], col=col1[2], lty=lty1[4])
+points(t(sapply(1:length(plot.data1), function(m) {
+  subset(plot.data1[[m]], psel==cv.psel1[m])})), 
+  col=rep(col1[-c(1, 2)], each=3), pch=1)
+par(mfrow=c(1, 1))
+
+
+
+
+
+
+
+
+
 
--- code/rnaseq_lymph_node_metastasis.R
+++ code/rnaseq_lymph_node_metastasis.R
@@ -10,6 +10,16 @@ if(!("CoRF" %in% installed.packages())) {
                  auth_token=Sys.getenv("GITHUB_PAT"))
 }
 
+if(substr(system('git log -n 1 --format="%h %aN %s %ad"', intern=TRUE), 1, 7)!=
+   substr(packageDescription("gren")$GithubSHA1, 1, 7)) {
+  if(!("devtools" %in% installed.packages())) {
+    install.packages("devtools")
+  }
+  library(devtools)
+  install_github("magnusmunch/gren/rpackage", local=FALSE,
+                 auth_token=Sys.getenv("GITHUB_PAT"))
+}
+
 ### libraries
 library(CoRF)
 library(GRridge)
@@ -43,6 +53,7 @@ corpart0 <- grridge2gren(CreatePartition(CDT$Corrs, ngroup=5, decreasing=TRUE))
 #roepmanpart <- CDT$RoepmanGenes+1
 partLNM <- list(corr=corpart0, pv=pvpart0)
 
+################################### model 1 ####################################
 ### fitting the models
 nfolds <- 1
 set.seed(2018)
@@ -73,53 +84,49 @@ fit1.gel2 <- cv.grpreg(x, y, as.numeric(as.factor(paste(corpart0, pvpart0))),
 fit1.gel3 <- cv.grpreg(x, y, as.numeric(as.factor(paste(corpart0, pvpart0))), 
                        penalty="gel", family="binomial", alpha=0.95)
 
+save(fit1.grridge, fit1.gren1, fit1.gren2, fit1.gren3, 
+     fit1.cmcp1, fit1.cmcp2, fit1.cmcp3, fit1.gel1, fit1.gel2,
+     fit1.gel3, file="results/rnaseq_lymph_node_metastasis_fit1.Rdata")
+
 ### obtaining predictions for test data
 ytest <- as.numeric(levels(RespValidation))[RespValidation]
 xtest <- ValidationData[, selectgenes]
 pred1 <- data.frame(ridge=predict.grridge(fit1.grridge, t(xtest))[, 1], 
                     grridge=predict.grridge(fit1.grridge, t(xtest))[, 2],
-                    gren1=as.numeric(predict(fit1.gren1, xtest, 
-                                             type="groupreg")),
-                    gren2=as.numeric(predict(fit1.gren2, xtest, 
-                                             type="groupreg")), 
-                    gren3=as.numeric(predict(fit1.gren3, xtest, 
-                                             type="groupreg")), 
-                    enet1=as.numeric(predict(fit1.gren1, xtest, 
-                                             type="regular")),
-                    enet2=as.numeric(predict(fit1.gren2, xtest, 
-                                             type="regular")), 
-                    enet3=as.numeric(predict(fit1.gren3, xtest, 
-                                             type="regular")), 
-                    cmcp1=predict(fit1.cmcp1, xtest, type="response"), 
-                    cmcp2=predict(fit1.cmcp2, xtest, type="response"), 
-                    cmcp3=predict(fit1.cmcp2, xtest, type="response"),
-                    gel1=predict(fit1.gel1, xtest, type="response"), 
-                    gel2=predict(fit1.gel2, xtest, type="response"), 
-                    gel3=predict(fit1.gel3, xtest, type="response"))
+                    gren1=predict(fit1.gren1$freq.model$groupreg, xtest, 
+                                  type="response"),
+                    gren2=predict(fit1.gren2$freq.model$groupreg, xtest, 
+                                  type="response"), 
+                    gren3=predict(fit1.gren3$freq.model$groupreg, xtest, 
+                                  type="response"), 
+                    enet1=predict(fit1.gren1$freq.model$regular, xtest, 
+                                  type="response"),
+                    enet2=predict(fit1.gren2$freq.model$regular, xtest, 
+                                  type="response"), 
+                    enet3=predict(fit1.gren3$freq.model$regular, xtest, 
+                                  type="response"), 
+                    cmcp1=predict(fit1.cmcp1$fit, xtest, type="response"), 
+                    cmcp2=predict(fit1.cmcp2$fit, xtest, type="response"), 
+                    cmcp3=predict(fit1.cmcp2$fit, xtest, type="response"),
+                    gel1=predict(fit1.gel1$fit, xtest, type="response"), 
+                    gel2=predict(fit1.gel2$fit, xtest, type="response"), 
+                    gel3=predict(fit1.gel3$fit, xtest, type="response"))
 
-psel1 <- data.frame(ridge=p, grridge=p,
-                    gren1=sum(coef(fit1.gren1$freq.model$groupreg, 
-                                   s=fit1.gren1$lambda)[-1, ]!=0),
-                    gren2=sum(coef(fit1.gren2$freq.model$groupreg, 
-                                   s=fit1.gren2$lambda)[-1, ]!=0),
-                    gren3=sum(coef(fit1.gren3$freq.model$groupreg, 
-                                   s=fit1.gren3$lambda)[-1, ]!=0),
-                    enet1=sum(coef(fit1.gren1$freq.model$regular, 
-                                   s=fit1.gren1$lambda)[-1, ]!=0),
-                    enet2=sum(coef(fit1.gren2$freq.model$regular, 
-                                   s=fit1.gren2$lambda)[-1, ]!=0),
-                    enet3=sum(coef(fit1.gren3$freq.model$regular, 
-                                   s=fit1.gren3$lambda)[-1, ]!=0),
-                    cmcp1=sum(fit1.cmcp1$fit$beta[-1, fit1.cmcp1$min]!=0), 
-                    cmcp2=sum(fit1.cmcp2$fit$beta[-1, fit1.cmcp2$min]!=0), 
-                    cmcp3=sum(fit1.cmcp3$fit$beta[-1, fit1.cmcp3$min]!=0),
-                    gel1=sum(fit1.gel1$fit$beta[-1, fit1.gel1$min]!=0), 
-                    gel2=sum(fit1.gel1$fit$beta[-1, fit1.gel1$min]!=0), 
-                    gel3=sum(fit1.gel1$fit$beta[-1, fit1.gel1$min]!=0))
+psel1 <- c(ridge=p, grridge=p,
+           gren1=fit1.gren1$freq.model$groupreg$df,
+           gren2=fit1.gren2$freq.model$groupreg$df,
+           gren3=fit1.gren3$freq.model$groupreg$df,
+           enet1=fit1.gren1$freq.model$regular$df,
+           enet2=fit1.gren2$freq.model$regular$df,
+           enet3=fit1.gren3$freq.model$regular$df,
+           cmcp1=colSums(fit1.cmcp1$fit$beta[-1, ]!=0), 
+           cmcp2=colSums(fit1.cmcp2$fit$beta[-1, ]!=0), 
+           cmcp3=colSums(fit1.cmcp3$fit$beta[-1, ]!=0),
+           gel1=colSums(fit1.gel1$fit$beta[-1, ]!=0), 
+           gel2=colSums(fit1.gel1$fit$beta[-1, ]!=0), 
+           gel3=colSums(fit1.gel1$fit$beta[-1, ]!=0))
 
 res1 <- rbind(pred1, psel1)
 rownames(res1) <- c(paste0("pred", c(1:length(ytest))), 
                     paste0("psel", 1:nfolds))
 write.table(res1, file="results/rnaseq_lymph_node_metastasis_res1.csv")
-
-
