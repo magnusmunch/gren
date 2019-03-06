460205e5e6221c76bd6f2ce7499cb660e6d37765
magnusmunch <magnus_munch@hotmail.com>
2019-03-06 13:30:34 +0100

    simulation 1 now parallel

--- code/simulations.R
+++ code/simulations.R
@@ -20,6 +20,7 @@ library(foreach)
 library(doParallel)
 library(irr)
 library(pROC)
+library(mvtnorm)
 
 ################################# simulation 1 #################################
 n <- 100
@@ -41,57 +42,21 @@ b <- 2*p*beta.mean/sum(rg*f^(c(1:G) - 1))
 beta <- numeric(p)
 
 part <- rep(c(1:G), each=p/G)
-nreps <- 100
-psel1 <- kappa1 <- data.frame(enet1=matrix(NA, nrow=nreps, ncol=100),
-                              enet2=matrix(NA, nrow=nreps, ncol=100),
-                              enet3=matrix(NA, nrow=nreps, ncol=100),
-                              gren1=matrix(NA, nrow=nreps, ncol=100),
-                              gren2=matrix(NA, nrow=nreps, ncol=100),
-                              gren3=matrix(NA, nrow=nreps, ncol=100),
-                              sgl1=matrix(NA, nrow=nreps, ncol=20),
-                              sgl2=matrix(NA, nrow=nreps, ncol=20),
-                              sgl3=matrix(NA, nrow=nreps, ncol=20),
-                              cmcp1=matrix(NA, nrow=nreps, ncol=100),
-                              cmcp2=matrix(NA, nrow=nreps, ncol=100),
-                              cmcp3=matrix(NA, nrow=nreps, ncol=100),
-                              gel1=matrix(NA, nrow=nreps, ncol=100),
-                              gel2=matrix(NA, nrow=nreps, ncol=100),
-                              gel3=matrix(NA, nrow=nreps, ncol=100))
-auc1 <- mse1 <- briers1 <- data.frame(ridge=numeric(nreps), 
-                                      grridge=numeric(nreps),
-                                      enet1=matrix(NA, nrow=nreps, ncol=100),
-                                      enet2=matrix(NA, nrow=nreps, ncol=100),
-                                      enet3=matrix(NA, nrow=nreps, ncol=100),
-                                      gren1=matrix(NA, nrow=nreps, ncol=100),
-                                      gren2=matrix(NA, nrow=nreps, ncol=100),
-                                      gren3=matrix(NA, nrow=nreps, ncol=100),
-                                      sgl1=matrix(NA, nrow=nreps, ncol=20),
-                                      sgl2=matrix(NA, nrow=nreps, ncol=20),
-                                      sgl3=matrix(NA, nrow=nreps, ncol=20),
-                                      cmcp1=matrix(NA, nrow=nreps, ncol=100),
-                                      cmcp2=matrix(NA, nrow=nreps, ncol=100),
-                                      cmcp3=matrix(NA, nrow=nreps, ncol=100),
-                                      gel1=matrix(NA, nrow=nreps, ncol=100),
-                                      gel2=matrix(NA, nrow=nreps, ncol=100),
-                                      gel3=matrix(NA, nrow=nreps, ncol=100))
-lambdagest1 <- list(grridge=matrix(NA, nrow=nreps, ncol=G),
-                    gren1=matrix(NA, nrow=nreps, ncol=G),
-                    gren2=matrix(NA, nrow=nreps, ncol=G),
-                    gren3=matrix(NA, nrow=nreps, ncol=G))
+nreps <- 1
 
 ### analysis splits in parallel
 ncores <- min(detectCores() - 1, nreps)
 cluster <- makeCluster(ncores, type="FORK")
 registerDoParallel(cluster)
 
-out <- foreach(k=c(1:nreps)) %dopar% {
+out <- foreach(k=c(1:nreps)) %do% {
   set.seed(2019 + k)
   
   for(g in 1:G) {
     idz <- c(((g-1)*p/G + 1):(g*p/G - rg))
     ida <- c(((g*p/G - rg) + 1):(g*p/G))
     beta[idz] <- 0
-    beta[ida] <- runif(rg, 0, b1*f^(g-1))
+    beta[ida] <- runif(rg, 0, b*f^(g-1))
   }
   xtrain <- rmvnorm(n, rep(0, p), Sigma)
   ytrain <- rbinom(n, 1, as.numeric(1/(1 + exp(-xtrain %*% beta))))
@@ -150,121 +115,59 @@ out <- foreach(k=c(1:nreps)) %dopar% {
   const <- sum((ytest - mean(ytest))^2)
   briers1 <- apply(pred1, 2, function(s) {1 - sum((ytest - s)^2)/const})
   
-  const <- sum((ytest - mean(ytest))^2)
-  briers1$ridge[r] <- 1 - sum((ytest - pred1.ridge)^2)/const
+  coef1 <- data.frame(ridge=coef(fit1.grridge$predobj$NoGroups),
+                      grridge=coef(fit1.grridge$predobj$GroupRegul),
+                      gren1=as.matrix(coef(fit1.gren1, type="groupreg")),
+                      gren2=as.matrix(coef(fit1.gren2, type="groupreg")),
+                      gren3=as.matrix(coef(fit1.gren3, type="groupreg")),
+                      enet1=as.matrix(coef(fit1.gren1, type="regular")),
+                      enet2=as.matrix(coef(fit1.gren2, type="regular")),
+                      enet3=as.matrix(coef(fit1.gren3, type="regular")),
+                      sgl1=rbind(fit1.sgl1$intercept, fit1.sgl1$beta),
+                      sgl2=rbind(fit1.sgl2$intercept, fit1.sgl2$beta),
+                      sgl3=rbind(fit1.sgl3$intercept, fit1.sgl3$beta),
+                      cmcp1=coef(fit1.cmcp1),
+                      cmcp2=coef(fit1.cmcp2),
+                      cmcp3=coef(fit1.cmcp3),
+                      gel1=coef(fit1.gel1),
+                      gel2=coef(fit1.gel2),
+                      gel3=coef(fit1.gel3))
   
-  coef(fit1.gren1, type="groupreg")
-  str(coef(fit1.gren1$freq.model$groupreg))
+  mse1 <- apply(coef1, 2, function(s) {mean((s - c(0, beta))^2)})
   
-  mse1$ridge[r] <- mean((coef(fit1.ridge)[-1] - beta)^2)
+  kappa1 <- apply(coef1, 2, function(s) {
+    kappa2(cbind(s[-1]!=0, beta!=0))$value})
   
-  mse1$grridge[r, ] <- sapply(fit1.grridge, function(s) {
-    mean((replace(rep(0, p), s$resEN$whichEN, s$resEN$betasEN) - beta)^2)})
+  psel1 <- c(ridge=p, grridge=p, 
+             gren1=fit1.gren1$freq.model$groupreg$df,
+             gren2=fit1.gren2$freq.model$groupreg$df,
+             gren3=fit1.gren3$freq.model$groupreg$df,
+             enet1=fit1.gren1$freq.model$regular$df,
+             enet2=fit1.gren2$freq.model$regular$df,
+             enet3=fit1.gren3$freq.model$regular$df,
+             sgl1=apply(fit1.sgl1$beta, 2, function(b) {sum(b!=0)}),
+             sgl2=apply(fit1.sgl2$beta, 2, function(b) {sum(b!=0)}),
+             sgl3=apply(fit1.sgl3$beta, 2, function(b) {sum(b!=0)}),
+             cmcp1=apply(as.matrix(coef(fit1.cmcp1)), 2, function(b) {
+               sum(b!=0)}),
+             cmcp2=apply(as.matrix(coef(fit1.cmcp2)), 2, function(b) {
+               sum(b!=0)}),
+             cmcp3=apply(as.matrix(coef(fit1.cmcp3)), 2, function(b) {
+               sum(b!=0)}),
+             gel1=apply(as.matrix(coef(fit1.gel1)), 2, function(b) {sum(b!=0)}),
+             gel2=apply(as.matrix(coef(fit1.gel2)), 2, function(b) {sum(b!=0)}),
+             gel3=apply(as.matrix(coef(fit1.gel3)), 2, function(b) {sum(b!=0)}))
   
-  mse1$gren1[r, ] <- apply(fit1.gren1$freq.model$groupreg$beta, 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$gren2[r, ] <- apply(fit1.gren2$freq.model$groupreg$beta, 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$gren3[r, ] <- apply(fit1.gren3$freq.model$groupreg$beta, 2, function(b) {
-    mean((b - beta)^2)})
+  mults1 <- c(grridge=fit1.grridge$lambdamults$part,
+              gren1=fit1.gren1$lambdag$part,
+              gren2=fit1.gren2$lambdag$part,
+              gren3=fit1.gren3$lambdag$part)
   
-  mse1$enet1[r, ] <- apply(fit1.gren1$freq.model$regular$beta, 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$enet2[r, ] <- apply(fit1.gren2$freq.model$regular$beta, 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$enet3[r, ] <- apply(fit1.gren3$freq.model$regular$beta, 2, function(b) {
-    mean((b - beta)^2)})
-  
-  mse1$sglasso1[r, ] <- apply(fit1.sglasso1$beta, 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$sglasso2[r, ] <- apply(fit1.sglasso2$beta, 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$sglasso3[r, ] <- apply(fit1.sglasso3$beta, 2, function(b) {
-    mean((b - beta)^2)})
-  
-  mse1$cmcp1[r, ] <- apply(fit1.cmcp1$beta[-1, ], 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$cmcp2[r, ] <- apply(fit1.cmcp2$beta[-1, ], 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$cmcp3[r, ] <- apply(fit1.cmcp3$beta[-1, ], 2, function(b) {
-    mean((b - beta)^2)})
-  
-  mse1$gelasso1[r, ] <- apply(fit1.gelasso1$beta[-1, ], 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$gelasso2[r, ] <- apply(fit1.gelasso2$beta[-1, ], 2, function(b) {
-    mean((b - beta)^2)})
-  mse1$gelasso3[r, ] <- apply(fit1.gelasso3$beta[-1, ], 2, function(b) {
-    mean((b - beta)^2)})
-  
-  kappa1$grridge[r, ] <- sapply(fit1.grridge, function(s) {
-    kappa2(cbind(beta!=0, replace(rep(FALSE, p), s$resEN$whichEN,
-                                  TRUE)))$value})
-  
-  kappa1$gren1[r, ] <- apply(fit1.gren1$freq.model$groupreg$beta, 2,
-                             function(b) {kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$gren2[r, ] <- apply(fit1.gren2$freq.model$groupreg$beta, 2,
-                             function(b) {kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$gren3[r, ] <- apply(fit1.gren3$freq.model$groupreg$beta, 2,
-                             function(b) {kappa2(cbind(beta!=0, b!=0))$value})
-  
-  kappa1$enet1[r, ] <- apply(fit1.gren1$freq.model$regular$beta, 2,
-                             function(b) {kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$enet2[r, ] <- apply(fit1.gren2$freq.model$regular$beta, 2,
-                             function(b) {kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$enet3[r, ] <- apply(fit1.gren3$freq.model$regular$beta, 2,
-                             function(b) {kappa2(cbind(beta!=0, b!=0))$value})
-  
-  kappa1$sglasso1[r, ] <- apply(fit1.sglasso1$beta, 2, function(b) {
-    kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$sglasso2[r, ] <- apply(fit1.sglasso2$beta, 2, function(b) {
-    kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$sglasso3[r, ] <- apply(fit1.sglasso3$beta, 2, function(b) {
-    kappa2(cbind(beta!=0, b!=0))$value})
-  
-  kappa1$cmcp1[r, ] <- apply(fit1.cmcp1$beta[-1, ], 2, function(b) {
-    kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$cmcp2[r, ] <- apply(fit1.cmcp2$beta[-1, ], 2, function(b) {
-    kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$cmcp3[r, ] <- apply(fit1.cmcp3$beta[-1, ], 2, function(b) {
-    kappa2(cbind(beta!=0, b!=0))$value})
-  
-  kappa1$gelasso1[r, ] <- apply(fit1.gelasso1$beta[-1, ], 2, function(b) {
-    kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$gelasso2[r, ] <- apply(fit1.gelasso2$beta[-1, ], 2, function(b) {
-    kappa2(cbind(beta!=0, b!=0))$value})
-  kappa1$gelasso3[r, ] <- apply(fit1.gelasso3$beta[-1, ], 2, function(b) {
-    kappa2(cbind(beta!=0, b!=0))$value})
-  
-  psel1$grridge[r, ] <- sapply(fit1.grridge, function(s) {
-    length(s$resEN$whichEN)})
-  
-  psel1$gren1[r, ] <- fit1.gren1$freq.model$groupreg$df
-  psel1$gren2[r, ] <- fit1.gren2$freq.model$groupreg$df
-  psel1$gren3[r, ] <- fit1.gren3$freq.model$groupreg$df
-  
-  psel1$enet1[r, ] <- fit1.gren1$freq.model$regular$df
-  psel1$enet2[r, ] <- fit1.gren2$freq.model$regular$df
-  psel1$enet3[r, ] <- fit1.gren3$freq.model$regular$df
-  
-  psel1$sglasso1[r, ] <- apply(fit1.sglasso1$beta, 2, function(b) {sum(b!=0)})
-  psel1$sglasso2[r, ] <- apply(fit1.sglasso2$beta, 2, function(b) {sum(b!=0)})
-  psel1$sglasso3[r, ] <- apply(fit1.sglasso3$beta, 2, function(b) {sum(b!=0)})
-  
-  psel1$cmcp1[r, ] <- apply(fit1.cmcp1$beta, 2, function(b) {sum(b!=0)})
-  psel1$cmcp2[r, ] <- apply(fit1.cmcp2$beta, 2, function(b) {sum(b!=0)})
-  psel1$cmcp3[r, ] <- apply(fit1.cmcp3$beta, 2, function(b) {sum(b!=0)})
-  
-  psel1$gelasso1[r, ] <- apply(fit1.gelasso1$beta, 2, function(b) {sum(b!=0)})
-  psel1$gelasso2[r, ] <- apply(fit1.gelasso2$beta, 2, function(b) {sum(b!=0)})
-  psel1$gelasso3[r, ] <- apply(fit1.gelasso3$beta, 2, function(b) {sum(b!=0)})
-  
-  lambdagest1$grridge[r, ] <- fit1.grridge[[1]]$lambdamults$groups
-  lambdagest1$gren1[r, ] <- fit1.gren1$lambdag$groups
-  lambdagest1$gren2[r, ] <- fit1.gren2$lambdag$groups
-  lambdagest1$gren3[r, ] <- fit1.gren3$lambdag$groups
-  
-  results1 <- list(auc=auc1, briers=briers1, mse=mse1, kappa=kappa1, psel=psel1,
-                   lambdag=lambdagest1)
-  save(results1, file=paste(path.res, "gren_sim1_res1.Rdata", sep=""))
+  list(auc=auc1, briers=briers1, mse=mse1, kappa=kappa1, mults=mults1)
   
 }
+
+
+results1 <- list(auc=auc1, briers=briers1, mse=mse1, kappa=kappa1, psel=psel1,
+                 lambdag=lambdagest1)
+save(results1, file=paste(path.res, "gren_sim1_res1.Rdata", sep=""))
