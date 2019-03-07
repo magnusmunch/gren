188035a6411c4c95a1a2629892cc054587543fdc
magnusmunch <magnus_munch@hotmail.com>
2019-03-07 14:05:01 +0100

    included briers in micrornaseq_cervical_cancer.R

--- code/micrornaseq_cervical_cancer.R
+++ code/micrornaseq_cervical_cancer.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-### installation of packages
+### installation of gren if updated on github
 if(substr(system('git log -n 1 --format="%h %aN %s %ad"', intern=TRUE), 1, 7)!=
    substr(packageDescription("gren")$GithubSHA1, 1, 7)) {
   if(!("devtools" %in% installed.packages())) {
@@ -18,7 +18,7 @@ library(grpreg)
 library(SGL)
 
 ### load data
-load("old_data/mirsData.RData")
+load("data/mirsData.RData")
 
 ### filter out the features with less than 2 unique counts
 unique.counts <- apply(t(as.matrix(mirsData$countData)), 2, function(s) {
@@ -45,18 +45,18 @@ part <- conservation
 p <- ncol(x)
 n <- nrow(x)
 
-psel <- c(seq(1, 5, 1), seq(7, 15, 2), seq(18, 30, 3), seq(34, 50, 4))
+csel <- c(seq(1, 5, 1), seq(7, 15, 2), seq(18, 30, 3), seq(34, 50, 4))
 
-fit1.gren1 <- gren(x, y, partitions=list(part=part), alpha=0.05, 
-                   standardize=TRUE, trace=FALSE, psel=psel)
-fit1.gren2 <- gren(x, y, partitions=list(part=part), alpha=0.5, 
-                   standardize=TRUE, trace=FALSE, psel=psel)
-fit1.gren3 <- gren(x, y, partitions=list(part=part), alpha=0.95, 
-                   standardize=TRUE, trace=FALSE, psel=psel)
+fit.gren1 <- gren(x, y, partitions=list(part=part), alpha=0.05, 
+                  standardize=TRUE, trace=FALSE, psel=csel)
+fit.gren2 <- gren(x, y, partitions=list(part=part), alpha=0.5, 
+                  standardize=TRUE, trace=FALSE, psel=csel)
+fit.gren3 <- gren(x, y, partitions=list(part=part), alpha=0.95, 
+                  standardize=TRUE, trace=FALSE, psel=csel)
 
-fit1.grridge <- grridge(t(x), y, list(part=split(1:p, part)))
+fit.grridge <- grridge(t(x), y, list(part=split(1:p, part)))
 
-save(fit1.grridge, fit1.gren1, fit1.gren2, fit1.gren3, 
+save(fit.grridge, fit.gren1, fit.gren2, fit.gren3, 
      file="results/micrornaseq_cervical_cancer_fit1.Rdata")
 
 nfolds <- 5
@@ -64,13 +64,13 @@ foldid <- sample(rep(1:nfolds, times=round(c(rep(
   n %/% nfolds + as.numeric((n %% nfolds)!=0), times=n %% nfolds),
   rep(n %/% nfolds, times=nfolds - n %% nfolds)))))
 
-pred1 <- as.data.frame(matrix(NA, nrow=n, ncol=6*length(psel) + 2))
-psel1 <- as.data.frame(matrix(NA, nrow=nfolds, ncol=6*length(psel) + 2))
-colnames(pred1) <- colnames(psel1) <- 
-  c("ridge", "grridge", paste0("gren1.psel", psel),
-    paste0("gren2.psel", psel), paste0("gren3.psel", psel),
-    paste0("enet1.psel", psel), paste0("enet2.psel", psel), 
-    paste0("enet3.psel", psel))
+pred <- as.data.frame(matrix(NA, nrow=n, ncol=6*length(csel) + 2))
+psel <- as.data.frame(matrix(NA, nrow=nfolds, ncol=6*length(csel) + 2))
+colnames(pred) <- colnames(psel) <- 
+  c("ridge", "grridge", paste0("gren1.psel", csel),
+    paste0("gren2.psel", csel), paste0("gren3.psel", csel),
+    paste0("enet1.psel", csel), paste0("enet2.psel", csel), 
+    paste0("enet3.psel", csel))
 
 for(k in sort(unique(foldid))) {
   cat(paste("fold ", k, "\n"))
@@ -82,51 +82,47 @@ for(k in sort(unique(foldid))) {
   ytrain <- y[foldid!=k]
   
   cv.gren1 <- gren(xtrain, ytrain, partitions=list(part=part), alpha=0.05, 
-                   standardize=TRUE, trace=FALSE, psel=psel)
+                   standardize=TRUE, trace=FALSE, psel=csel)
   cv.gren2 <- gren(xtrain, ytrain, partitions=list(part=part), alpha=0.5, 
-                   standardize=TRUE, trace=FALSE, psel=psel)
+                   standardize=TRUE, trace=FALSE, psel=csel)
   cv.gren3 <- gren(xtrain, ytrain, partitions=list(part=part), alpha=0.95, 
-                   standardize=TRUE, trace=FALSE, psel=psel)
+                   standardize=TRUE, trace=FALSE, psel=csel)
   
   cv.grridge <-  grridge(t(xtrain), ytrain, list(part=split(1:p, part)))
 
-  pred1$ridge[foldid==k] <- predict.grridge(cv.grridge, t(xtest))[, 1]
-  pred1$grridge[foldid==k] <- predict.grridge(cv.grridge, t(xtest))[, 2]
+  pred$ridge[foldid==k] <- predict.grridge(cv.grridge, t(xtest))[, 1]
+  pred$grridge[foldid==k] <- predict.grridge(cv.grridge, t(xtest))[, 2]
   
-  pred1[foldid==k, grep("gren1", colnames(pred1))] <- 
-    predict(cv.gren1$freq.model$groupreg, xtest, type="response")
-  pred1[foldid==k, grep("gren2", colnames(pred1))] <- 
-    predict(cv.gren2$freq.model$groupreg, xtest, type="response")
-  pred1[foldid==k, grep("gren3", colnames(pred1))] <- 
-    predict(cv.gren3$freq.model$groupreg, xtest, type="response")
+  pred[foldid==k, grep("gren1", colnames(pred))] <- 
+    predict(cv.gren1, xtest, type="groupreg")
+  pred[foldid==k, grep("gren2", colnames(pred))] <- 
+    predict(cv.gren2, xtest, type="groupreg")
+  pred[foldid==k, grep("gren3", colnames(pred))] <- 
+    predict(cv.gren3, xtest, type="groupreg")
   
-  pred1[foldid==k, grep("enet1", colnames(pred1))] <- 
-    predict(cv.gren1$freq.model$regular, xtest, type="response")
-  pred1[foldid==k, grep("enet2", colnames(pred1))] <- 
-    predict(cv.gren2$freq.model$regular, xtest, type="response")
-  pred1[foldid==k, grep("enet3", colnames(pred1))] <- 
-    predict(cv.gren3$freq.model$regular, xtest, type="response")
+  pred[foldid==k, grep("enet1", colnames(pred))] <- 
+    predict(cv.gren1, xtest, type="regular")
+  pred[foldid==k, grep("enet2", colnames(pred))] <- 
+    predict(cv.gren2, xtest, type="regular")
+  pred[foldid==k, grep("enet3", colnames(pred))] <- 
+    predict(cv.gren3, xtest, type="regular")
 
-  psel1$ridge <- psel1$grridge <- p
+  psel$ridge <- psel$grridge <- p
   
-  psel1[k, grep("gren1", colnames(psel1))] <- cv.gren1$freq.model$groupreg$df
-  psel1[k, grep("gren2", colnames(psel1))] <- cv.gren2$freq.model$groupreg$df
-  psel1[k, grep("gren3", colnames(psel1))] <- cv.gren3$freq.model$groupreg$df
+  psel[k, grep("gren1", colnames(psel))] <- cv.gren1$freq.model$groupreg$df
+  psel[k, grep("gren2", colnames(psel))] <- cv.gren2$freq.model$groupreg$df
+  psel[k, grep("gren3", colnames(psel))] <- cv.gren3$freq.model$groupreg$df
   
-  psel1[k, grep("enet1", colnames(psel1))] <- cv.gren1$freq.model$regular$df
-  psel1[k, grep("enet2", colnames(psel1))] <- cv.gren2$freq.model$regular$df
-  psel1[k, grep("enet3", colnames(psel1))] <- cv.gren3$freq.model$regular$df
+  psel[k, grep("enet1", colnames(psel))] <- cv.gren1$freq.model$regular$df
+  psel[k, grep("enet2", colnames(psel))] <- cv.gren2$freq.model$regular$df
+  psel[k, grep("enet3", colnames(psel))] <- cv.gren3$freq.model$regular$df
   
 }
 
-auc1 <- apply(pred1, 2, function(s) {pROC::auc(y, s)})
-res1 <- rbind(pred1, psel1, auc1)
-rownames(res1) <- c(paste0("pred", c(1:length(y))), paste0("psel", c(1:nfolds)),
-                    paste0("auc", 1))
-write.table(res1, file="results/micrornaseq_cervical_cancer_res1.csv")
-
-
-
-
-
-
+auc <- apply(pred, 2, function(s) {pROC::auc(y, s)})
+briers <- apply(pred, 2, function(m) {
+  1 - mean((m - y)^2)/mean((mean(y) - y)^2)})
+res <- rbind(pred, psel, auc, briers)
+rownames(res) <- c(paste0("pred", c(1:length(y))), paste0("psel", c(1:nfolds)),
+                   paste0("auc", 1), paste0("briers", 1))
+write.table(res, file="results/micrornaseq_cervical_cancer_res1.csv")
